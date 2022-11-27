c6ebe51b70902243fce2688f314c7d9a54a29a5f
bkeener <bkeener7@gmail.com>
2022-11-27 09:00:09 -0700

    Setup tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,7 +23,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -77,6 +77,8 @@ RSpec.configure do |config|
       example.run
     end
   end
+
+  config.include RequestSpecHelper, type: :request
 
   # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
   Shoulda::Matchers.configure do |config|
